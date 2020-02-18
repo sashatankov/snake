9ffea31a3dc3115f07fe6b8cc01ddab4ea9bd51d
Alexandr Tankov <tank10000@cs.huji.ac.il>
2020-02-18 13:37:54 +0200

    gradient added

--- policies/my_policy.py
+++ policies/my_policy.py
@@ -7,7 +7,7 @@ from policies.snake_model import SnakeModel
 class MyPolicy(bp.Policy):
 
     def init_run(self):
-        self.actions = {a: i for i, a in enumerate(MyPolicy.ACTIONS)}
+        self.actions2i = {a: i for i, a in enumerate(MyPolicy.ACTIONS)}
         self.model = SnakeModel()
         self.loss_object = tf.keras.losses.MeanSquaredError()
         self.train_loss = tf.keras.metrics.Mean()
@@ -18,12 +18,25 @@ class MyPolicy(bp.Policy):
         pass
 
     def learn(self, round, prev_state, prev_action, reward, new_state, too_slow):
-        # TODO train_step() call goes here
-        pass
+
+        learning_rate = 0.01
+        discount_factor = 0.9
+        with tf.GradientTape() as tape:
+            pred_q = self._q_value(prev_state, prev_action)
+            target_q = reward + discount_factor * tf.max(self.model(new_state))
+            loss = self.loss_object(pred_q - target_q)
+        gradients = tape.gradient(loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
 
     def act(self, round, prev_state, prev_action, reward, new_state, too_slow):
-        # TODO run the state thorough the network and argmax of the result
-        pass
+
+        preds = self.model(new_state)
+        i = tf.argmax(preds)
+        return self.ACTIONS[i]
+
+    def _q_value(self, state, action):
+        pred = self.model(state)
+        return pred[self.actions2i[action]]
 
 
 @tf.function
